90fb5f91c44e8c59875095361a5eff469fdb8d61
ElijahMoodyEKU <elijah_moody6@mymail.eku.edu>
2026-02-26 11:52:59 -0500

    Initial Commit

---
+++ M6Initializer_Moody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M6Initializer_Moody
+//
+//  Created by Moody, Elijah M. on 2/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
